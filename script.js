063ff16c9c17db365c7b0aebf43120900af0439e
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2024-04-23 00:06:45 +0300

    Added function changeColor

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const busesFromLvivList = './data/buses-from-lviv.json';
+const busesToLvivList = './data/buses-to-lviv.json';
+
 async function loadBusesFromLviv() {
     try {
-        const server = './data/buses-from-lviv.json';
+        const server = busesFromLvivList;
         const response = await fetch(server);
         const responseResult = await response.json();
         let buses = responseResult.buses;
@@ -20,34 +23,49 @@ function displayBusesFromLviv(buses) {
         const oneBus = document.createElement('tr');
         oneBus.classList.add('table-row');
         oneBus.innerHTML = `
-            <td class="table-number">${el.number}</td>
+            <td class="table-number" style="color:${changeColor(el.number)};">${el.number}</td>
             <td class="table-time">${el.time}</td>
         `
         tableBody.appendChild(oneBus);
-        // let bus = document.querySelectorAll(".table-number");
-        // directionColors(el.number, bus);
-    });
-    
+    });  
 };
 
-// function directionColors(number, bus) {
-//     for (let i = 0; i < bus.length; i++) {
-//         switch (number) {
-//         case "113":
-//             i.style.color = "red"
-//             break;
-//         case "603":
-//             i.style.color = "blue"
-//             break;
-//         default:
-//             break;
-//         }
-//         console.log(bus);
-        
-//     }
-
-//     console.log(number);
-//     console.log(bus);
-// };
+function changeColor(busNumber) {
+    switch (busNumber) {
+        case "109":
+            return "#b3001b";
+            break;
+        case "113":
+            return "#febe10";
+            break;
+        case "113 A":
+            return "#a770a0";
+            break;
+        case "118":
+            return "#767b63";
+            break;
+        case "132":
+            return "#ff7f00";
+            break;
+        case "202":
+            return "#007d6a";
+            break;
+        case "603":
+            return "#2e8b57";
+            break;
+        case "604 A":
+            return "#5c8cbf";
+            break;
+        case "604 Б":
+            return "#627478";
+            break;
+        case "614":
+            return "#2b57ac";
+            break;
+        default:
+            return "#33302a";
+            break;
+        }
+}
 
 loadBusesFromLviv();
